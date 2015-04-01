3a4cd6a00b85347c75bbb3c3ff42f1a7a21fd066
Etienne Adam <etienne.adam@withings.com>
2015-04-01 16:44:11 +0200

    change detection

--- start_cassandra.sh
+++ start_cassandra.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-pgrep -f org.apache.cassandra.service.CassandraDaemon >/dev/null
+#pgrep -f org.apache.cassandra.service.CassandraDaemon >/dev/null # not working with long cmd line
+pgrep -f 'Dcassandra-pidfile=/var/run/cassandra/cassandra.pid' >/dev/null
 
 if [ $? == 1 ]; then
   echo "cassandra not started"
