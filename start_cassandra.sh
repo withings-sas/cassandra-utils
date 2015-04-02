075d331b1b907e7cacd749b00a57e76bf694eccf
Etienne Adam <etienne.adam@withings.com>
2015-04-02 09:04:48 +0200

    add PATH (to fix issue with our configs)

--- start_cassandra.sh
+++ start_cassandra.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
+
 #pgrep -f org.apache.cassandra.service.CassandraDaemon >/dev/null # not working with long cmd line
 pgrep -f 'Dcassandra-pidfile=/var/run/cassandra/cassandra.pid' >/dev/null
 
