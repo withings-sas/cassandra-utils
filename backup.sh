d8abe7a9fdd8573d88dea8542b76ca3c5783197b
Etienne Adam <etienne.adam@withings.com>
2015-04-28 08:52:56 +0200

    add hash of CF in backup filename

--- backup.sh
+++ backup.sh
@@ -50,6 +50,8 @@ nodetool clearsnapshot
 
 for keyspacename in $DBS
 do
+  REMOTEFULLPATH="$REMOTEFOLDER/`hostname`/$BACKUPDATE/$keyspacename"
+  ssh $REMOTEHOST "mkdir -p $REMOTEFULLPATH"
   echo `date +%Y-%m-%dT%H:%M:%S`" Snapshoting..."
   nodetool snapshot $keyspacename
   echo `date +%Y-%m-%dT%H:%M:%S`" Done. Moving snapshots to backup..."
@@ -59,12 +61,10 @@ do
     for snap in $keyspacepath*/snapshots/*
     do
       if [[ $snap =~ snapshots\/[0-9]{13}$ ]]; then
-        tablename=$(echo "$snap" | sed -r 's/.*\/(.*)-[a-f0-9]{32}\/snapshots\/[0-9]{13}/\1/')
-        echo `date +%Y-%m-%dT%H:%M:%S`" snap folder:[$snap] tablename:[$tablename]"
-        REMOTEFULLPATH="$REMOTEFOLDER/`hostname`/$BACKUPDATE/$keyspacename"
-        ssh $REMOTEHOST "mkdir -p $REMOTEFULLPATH"
-        echo `date +%Y-%m-%dT%H:%M:%S`" tar -C $snap -cf - . | pbzip2 -p3 | ssh $REMOTEHOST 'cat > $REMOTEFULLPATH/$tablename.tbz2'"
-        tar -C "$snap" -cf - . | pbzip2 -p2 | ssh $REMOTEHOST "cat > $REMOTEFULLPATH/$tablename.tbz2"
+        columnfamily=$(echo "$snap" | sed -r 's/.*\/(.*-[a-f0-9]{32})\/snapshots\/[0-9]{13}/\1/')
+        #echo `date +%Y-%m-%dT%H:%M:%S`" snap folder:[$snap] columnfamily:[$columnfamily]"
+        echo `date +%Y-%m-%dT%H:%M:%S`" tar -C $snap -cf - . | pbzip2 -p8 | ssh $REMOTEHOST 'cat > $REMOTEFULLPATH/$columnfamily.tbz2'"
+        tar -C "$snap" -cf - . | pbzip2 -p8 | ssh $REMOTEHOST "cat > $REMOTEFULLPATH/$columnfamily.tbz2"
       fi
     done
   done
